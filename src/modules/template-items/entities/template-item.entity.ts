b118f5b59aa8f85a369911ef8d9563d66d58e605
Muhammad Eman Ejaz <memanejaz97@gmail.com>
2023-04-17 15:23:14 +1000

    feat: Entity for template items

---
+++ src/modules/template-items/entities/template-item.entity.ts
@@ -0,0 +1,84 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import {
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Template } from '../../template/entities/template.entity';
+
+import { TemplateItemType } from '../enums';
+import { CustomBaseEntity } from 'src/common';
+
+@Entity({ name: 'template_items' })
+export class TemplateItem extends CustomBaseEntity {
+  @PrimaryGeneratedColumn({ unsigned: true })
+  id: number;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+    nullable: false,
+  })
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @Column({
+    type: 'enum',
+    enum: TemplateItemType,
+    nullable: false,
+  })
+  @IsNotEmpty()
+  @IsEnum(TemplateItemType)
+  type: TemplateItemType;
+
+  @Column({
+    name: 'template_id',
+    type: 'int',
+    nullable: false,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  templateId: number;
+
+  @Column({
+    name: 'order_index',
+    type: 'int',
+    nullable: false,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  orderIndex: number;
+
+  @Column({
+    name: 'parent_id',
+    type: 'int',
+    nullable: true,
+  })
+  @IsOptional()
+  @IsNumber()
+  parentId?: number;
+
+  @Column({
+    type: 'boolean',
+    nullable: true,
+  })
+  @IsOptional()
+  @IsBoolean()
+  status?: boolean;
+
+  @ManyToOne(() => Template, (template) => template.templateItems)
+  @JoinColumn({
+    name: 'template_id',
+  })
+  template: Template;
+}
